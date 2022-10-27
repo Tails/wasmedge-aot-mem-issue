7314c48ee7a75617270c6476cf43f0bd0f7e8129
Luuk de Waal Malefijt <luuk@waalmalefijt.nl>
2022-10-27 14:24:31 +0200

    delete deps, flatten to single lib file

--- prerendering/build.rs
+++ prerendering/build.rs
@@ -9,44 +9,10 @@ use std::process::Command;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-// todo: resolve this from parture folder helper path util
-const WASM_BLOB_PATH : &str
-    = "../prerendering_wasm/dist/prerendering_wasm.wasm";
-
-const AOT_OUTPUT_PATH : &str
-    = "../prerendering_wasm/dist/prerendering_wasm_aot.so";
-
-const OPT_LEVEL: CompilerOptimizationLevel
-    = CompilerOptimizationLevel::O0;
-
-// const WASM_BLOB : &[u8] = include_bytes!("../partage_prerendering_wasm/partage_prerendering_wasm.wasm");
-
-// create vm config
-fn config() -> Config {
-    let common_options = CommonConfigOptions::default();
-
-    let compiler_options = CompilerConfigOptions::default()
-        .dump_ir(false)
-        .generic_binary(false)
-        .interruptible(false)
-        .optimization_level(OPT_LEVEL)
-        .out_format(CompilerOutputFormat::Native)
-        ;
-
-    let host_reg_options = HostRegistrationConfigOptions::new()
-        .wasi(true);
-
-    let result = ConfigBuilder::new(common_options)
-        .with_host_registration_config(host_reg_options)
-        .with_compiler_config(compiler_options)
-        .build();
-
-    assert!(result.is_ok());
-    result.unwrap()
-}
+use anyhow::Result;
 
 /// build the input WASM (interpreted)
-fn build_wasm() -> WasmEdgeResult<()> {
+fn build_wasm() -> anyhow::Result<()> {
     println!("building standard wasm binary...");
 
     // tell cargo to invalidate build cache if the _wasm package changes
@@ -58,8 +24,7 @@ fn build_wasm() -> WasmEdgeResult<()> {
     let res = Command::new("make")
         .current_dir("../prerendering_wasm")
         .arg("build")
-        .output()
-        .expect("Failed to execute command");
+        .output()?;
 
     if !res.status.success() {
         panic!("{}", String::from_utf8(res.stderr).unwrap());
@@ -68,50 +33,6 @@ fn build_wasm() -> WasmEdgeResult<()> {
     Ok(())
 }
 
-/// produce native optimized binary
-fn compile_sdk() -> WasmEdgeResult<()> {
-    println!("compiling AOT wasm binary through Rust SDK...");
-
-    std::fs::create_dir_all("../prerendering_wasm/dist/")
-        .expect("failed to make output dirs");
-
-    Compiler::new(Some(&config()))?
-        .compile_from_file(WASM_BLOB_PATH,  AOT_OUTPUT_PATH)
-        // .compile_from_bytes(WASM_BLOB, "./aot/aot.wasm")
-}
-
-/// produce native optimized binary
-fn compile_cli() {
-    println!("building standard wasm binary using wasmedge CLI...");
-
-    let res = Command::new("make")
-        .current_dir("../prerendering_wasm")
-        .arg("build-bin-aot")
-        .output()
-        .expect("Failed to execute command");
-
-    if !res.status.success() {
-        panic!("{}", String::from_utf8(res.stderr).unwrap());
-    }
-}
-
-fn compile() {
-    println!("cargo:rerun-if-changed=../prerendering_wasm/dist/prerenderer_wasm.wasm");
-
-    // do not compile here if we are not running --release because it takes ages
-    // #[cfg(not(debug_assertions))] {
-    //     compile_sdk();
-    // }
-    //
-    // // if we run in release mode, no need for external binary
-    // #[cfg(debug_assertions)] {
-    //     compile_cli();
-    // }
-
-    compile_sdk();
-}
-
 pub fn main() {
-    build_wasm().and_then(|()|
-        Ok(compile()));
+    build_wasm().unwrap();
 }
--- prerendering/src/lib.rs
+++ prerendering/src/lib.rs
@@ -2,26 +2,123 @@
 #![feature(explicit_generic_args_with_impl_trait)]
 #![feature(never_type)]
 
-mod run;
+use wasmedge_sdk::{error::HostFuncError, Compiler, params, wat2wasm, CallingFrame, Executor, ImportObjectBuilder, Module, Store, WasmValue, config::Config, Vm, AsInstance, CompilerOutputFormat, CompilerOptimizationLevel, WasmEdgeResult, WasmValTypeList, ImportObject};
+use wasmedge_sdk::config::{ConfigBuilder, HostRegistrationConfigOptions, CommonConfigOptions, CompilerConfigOptions};
+use wasmedge_sdk::WasmVal;
+use wasmedge_sys::Memory;
 
-pub const WASM_BLOB_INTERPRETED_PATH: &'static str
-    = "../prerendering_wasm/dist/prerendering_wasm.wasm";
+use std::env;
+use std::path::Path;
+use std::thread;
 
-pub const WASM_BLOB_AOT_PATH: &'static str
-    = "../prerendering_wasm/dist/prerendering_wasm_aot.so";
+use anyhow::anyhow;
 
-#[tokio::test]
-async fn test_interpreted() {
-    let interpreted_res = crate::run::run_wasm(
-        crate::WASM_BLOB_INTERPRETED_PATH).await;
+// like docker mounts for WASI, which dirs to mount
+const WASI_VOLUME_MOUNTS: &str = ".:.";
+
+// name of prerenderer wasm module
+const PRERENDERER_WASM_MODULE_NAME : &str = "prerenderer";
+
+// function to be called as entrypoint
+const PRERENDERER_WASM_MODULE_ENTRYPOINT : &str = "_start";
+
+// todo: resolve this from parture folder helper path util
+const WASM_BLOB_INTERPRETED_PATH : &str
+= "../prerendering_wasm/dist/prerendering_wasm.wasm";
+
+const WASM_BLOB_AOT_PATH : &str
+= "../prerendering_wasm/dist/prerendering_wasm_aot.so";
+
+const OPT_LEVEL: CompilerOptimizationLevel
+= CompilerOptimizationLevel::O0;
+
+// create vm config
+pub fn config() -> Config {
+    let common_options = CommonConfigOptions::default();
+
+    let compiler_options = CompilerConfigOptions::default()
+        .dump_ir(false)
+        .generic_binary(true)
+        .interruptible(false)
+        .optimization_level(OPT_LEVEL)
+        .out_format(CompilerOutputFormat::Native)
+        ;
+
+    let host_reg_options = HostRegistrationConfigOptions::new()
+        .wasi(true);
+
+    let result = ConfigBuilder::new(common_options)
+        .with_host_registration_config(host_reg_options)
+        .with_compiler_config(compiler_options)
+        .build();
+
+    assert!(result.is_ok());
+    result.unwrap()
+}
+
+/// produce native optimized binary
+fn compile_sdk() -> WasmEdgeResult<()> {
+    println!("compiling AOT wasm binary through Rust SDK...");
+
+    std::fs::create_dir_all("../prerendering_wasm/dist/")
+        .expect("failed to make output dirs");
+
+    Compiler::new(Some(&config()))?
+        .compile_from_file(
+            WASM_BLOB_INTERPRETED_PATH,
+            WASM_BLOB_AOT_PATH)
+}
+
+// create VM for running prerenderer code
+fn vm(file: &str) -> WasmEdgeResult<Vm> {
+    let mut vm = Vm::new(Some(config()))?;
+
+    // this is essential, without it the runtime cannot read files and will error out
+    // https://github.com/WasmEdge/WasmEdge/issues/1872
+    println!("configuring wasi...");
+    let mut wasi_module = vm.wasi_module()?;
+    wasi_module.initialize(None, None, Some(vec!(WASI_VOLUME_MOUNTS)));
+
+    // our code
+    let module = Module::from_file(Some(&crate::config()), file).unwrap();
+
+    // dbg info
+    for ex in &module.exports() {
+        println!("export name={}, ty={:#?}", ex.name(), ex.ty().unwrap());
+    }
+
+    vm
+        .register_module(Some(PRERENDERER_WASM_MODULE_NAME), module)
+}
+
+pub fn run_wasm(file: &str) -> anyhow::Result<()> {
+    // create a vm with the config settings
+    println!("creating vm...");
+    let vm = vm(file)?;
+
+    vm.run_func(
+        Some(PRERENDERER_WASM_MODULE_NAME),
+        PRERENDERER_WASM_MODULE_ENTRYPOINT,
+        params!()
+    )?;
+
+    Ok(())
+}
+
+#[test]
+fn test_interpreted() {
+    let interpreted_res = run_wasm(
+        crate::WASM_BLOB_INTERPRETED_PATH);
 
     assert!(interpreted_res.is_ok())
 }
 
-#[tokio::test]
-async fn test_aot() {
-    let aot_res = crate::run::run_wasm(
-        crate::WASM_BLOB_AOT_PATH).await;
+#[test]
+fn test_aot() {
+    compile_sdk().unwrap();
+
+    let aot_res = run_wasm(
+        crate::WASM_BLOB_AOT_PATH);
 
     assert!(aot_res.is_ok());
 }
